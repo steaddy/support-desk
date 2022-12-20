454a43037228beda6469518691f8f855a42b6de8
Steaddy <9265214314@mai.ru>
2022-12-20 11:00:41 +0300

    Login and Register slices

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import {FaSignInAlt} from "react-icons/fa";
+import {useSelector, useDispatch} from "react-redux";
+import {login} from "../features/auth/authSlice";
 import {toast} from "react-toastify";
 
 const Login = () => {
@@ -10,6 +12,11 @@ const Login = () => {
 
     const { password, email } = formData
 
+    const dispatch = useDispatch()
+
+    const { user, isLoading, isSuccess, message } = useSelector(state => state.auth)
+
+
     const onChange = (e) => {
         setFormData(prevState => ({
             ...prevState,
@@ -19,6 +26,13 @@ const Login = () => {
 
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const userData = {
+            email,
+            password,
+        }
+
+        dispatch(login(userData))
     }
 
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import {FaUser} from "react-icons/fa";
 import {toast} from "react-toastify";
+import {useSelector, useDispatch} from "react-redux";
+import {register} from "../features/auth/authSlice";
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -11,6 +13,10 @@ const Register = () => {
     })
 
     const {name, password, email, password2} = formData
+
+    const dispatch = useDispatch()
+
+    const { user, isLoading, isSuccess, message } = useSelector(state => state.auth)
 
     const onChange = (e) => {
         setFormData(prevState => ({
@@ -24,6 +30,14 @@ const Register = () => {
 
         if(password !== password2) {
             toast.error('Passwords do not match')
+        } else {
+            const userData = {
+                name,
+                email,
+                password,
+            }
+
+            dispatch(register(userData))
         }
     }
 
